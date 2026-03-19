4c54186bce97207d23f2fb23753c7cf135096b85
Alekmz <amalexmuniz@gmail.com>
2026-03-18 22:00:22 -0300

    feat: exam entity endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { prisma } from './prisma/prisma';
-import type { Usuario } from './prisma/generated/prisma/client';
+import type { Exame, Usuario } from './prisma/generated/prisma/client';
 
 const app = express();
 app.use(express.json())
@@ -71,6 +71,66 @@ app.delete('/usuarios/:id', async (req, res) => {
 })
 
 //Exames
+app.get('/exames', async (_, res) => {
+  const exames = await prisma.exame.findMany();
+  return res.json(exames);
+})
+
+app.get('/exames/:id', async (req, res) => {
+  const idExame = Number(req.params.id)
+  const exame = await prisma.exame.findUnique({
+    where: {
+      id: idExame
+    }
+  })
+
+  return res.status(200).json(exame);
+})
+
+app.post("/exames", async (req, res) => {
+  const dadosExame = req.body as Exame
+  const exameCriado = await prisma.exame.create({
+    data: {
+      tipo_exame: dadosExame.tipo_exame,
+      valor: dadosExame.valor,
+      descricao: dadosExame.descricao,
+      data_exame: new Date(dadosExame.data_exame),
+      resultado: dadosExame.resultado
+    }
+  })
+  return res.status(201).json(exameCriado)
+})
+
+app.put("/exames/:id", async (req, res) => {
+  const idExame = Number(req.params.id)
+  const dadosParaAtualizar = req.body as Omit<Exame, 'id'>
+
+  const exameAtualizado = await prisma.exame.update({
+    data: {
+      ...dadosParaAtualizar,
+      data_exame: new Date(dadosParaAtualizar.data_exame)
+    },
+    where: {
+      id: idExame
+    }
+  })
+
+  return res.status(200).json(exameAtualizado);
+})
+
+app.delete('/exames/:id', async (req, res) => {
+  const idExame = Number(req.params.id)
+  const exameDeletado = await prisma.exame.delete({
+    where: {
+      id: idExame
+    }
+  })
+
+  return res.status(200).json({
+    mensagem: "Exame deletado com sucesso!",
+    data: exameDeletado
+  });
+})
 
 app.listen(port, () => {
   console.log("Servidor ta de pé :p")
